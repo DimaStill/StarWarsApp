d0da620045129703715f3a109a9a2c4c4d7d25c0
Dmytro Karaiev <dimastill123@gmail.com>
2021-02-19 22:57:11 +0200

    Imported MatCardModule and MatIconModule to SharedModule

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule }   from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatIconModule
+  ],
+  exports: [
+    MatCardModule,
+    MatIconModule
   ]
 })
 export class SharedModule { }
