b12db9c5c716e8fe00a71a23b4d94f54c6d361eb
Dmytro Karaiev <dimastill123@gmail.com>
2021-02-19 21:07:22 +0200

    Added function for get residents of planet

--- src/app/core/services/people.service.ts
+++ src/app/core/services/people.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
+import { bufferCount, mergeMap, switchMap, tap } from 'rxjs/operators';
 import { IPeople } from 'src/app/shared/shared/models/people';
 
 @Injectable({
@@ -9,6 +10,15 @@ import { IPeople } from 'src/app/shared/shared/models/people';
 export class PeopleService {
 
   constructor(private readonly httpClient: HttpClient) { }
+
+  getResidentsOfPlanet(residentsReference: Array<string>) {
+    return from(residentsReference).pipe(
+      mergeMap(residentReference => 
+        this.getPeopleByReference(residentReference)
+      ),
+      bufferCount(residentsReference.length)
+    );
+  }
 
   getPeopleByReference(reference: string): Observable<IPeople> {
     return this.httpClient.get<IPeople>(reference);
